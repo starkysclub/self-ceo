9c1f54ddf1c58c567023e8ede8258d268bf27f37
Václav Brzezina <vaclav.brzezina@starkysclub.com>
2020-08-12 10:41:45 +0200

    debug

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,12 @@ import Section from './Section';
 import { Grid, Row, Col } from 'react-styled-flexboxgrid';
 import { SC } from '../styles/theme';
 import { withTranslation, WithTranslation } from '../app/i18n';
-import { useState } from 'react';
+import { FormikConfig } from 'formik';
+import Form from './Form';
+import Input from './Input';
+import Select from './Select';
+import TextArea from './TextArea';
+import { Button } from './Button';
 
 interface ContactTemplateProps extends WithTranslation {}
 
@@ -14,13 +19,11 @@ const encode = (data: any) => {
 };
 
 const ContactTemplate: SC<ContactTemplateProps> = ({ className, t }) => {
-  const [state, setState] = useState({ name: '', email: '', message: '' });
-
-  const handleSubmit = (e: any) => {
+  const onSubmit = (values: any) => {
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact', ...state }),
+      body: encode({ 'form-name': 'contact', ...values }),
     })
       .then(() => alert('Success!'))
       .catch((error) => alert(error));
@@ -28,7 +31,12 @@ const ContactTemplate: SC<ContactTemplateProps> = ({ className, t }) => {
     e.preventDefault();
   };
 
-  const handleChange = (e: any) => setState({ [e.target.name]: e.target.value } as any);
+  const initialValues = {};
+
+  const config: FormikConfig<any> = {
+    initialValues,
+    onSubmit,
+  };
 
   return (
     <Section className={className} id="contact">
@@ -43,26 +51,21 @@ const ContactTemplate: SC<ContactTemplateProps> = ({ className, t }) => {
             <div className="image" />
           </Col>
           <Col xs={12} md={6}>
-            <form onSubmit={handleSubmit}>
-              <p>
-                <label>
-                  Your Name: <input type="text" name="name" value={state.name} onChange={handleChange} />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Your Email: <input type="email" name="email" value={state.email} onChange={handleChange} />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Message: <textarea name="message" value={state.message} onChange={handleChange} />
-                </label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
-            </form>
+            <Form config={config} formProps={{ method: 'POST', 'data-netlify': true, action: 'contact' }}>
+              <input type="hidden" name="form-name" value="contact" />
+              <Input name="fullName" placeholder={t('contact.fields.name')} />
+              <Input name="email" placeholder={t('contact.fields.email')} />
+              <Select
+                name="topic"
+                options={[
+                  { label: t('contact.fields.options.bug-report'), value: t('contact.fields.options.bug-report') },
+                  { label: t('contact.fields.options.other'), value: t('contact.fields.options.other') },
+                ]}
+                placeholder={t('contact.fields.topic')}
+              />
+              <TextArea name="message" placeholder={t('contact.fields.message')} />
+              <Button>Send</Button>
+            </Form>
           </Col>
         </Row>
       </Grid>
